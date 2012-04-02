1c97cdad23d9acb2cb5406f6ac1d3856a6b64e95
Luis Forquesato <luisforque@gmail.com>
2012-04-02 14:41:05 -0300

    finish user gedit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def new
     @user = User.new
@@ -8,17 +11,53 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+		sign_in @user
 		flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+		redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def authenticate
+	deny_access unless signed_in?
+  end
+  
+  
+  def correct_user
+	@user = User.find(params[:id])
+	redirect_to(root_path) unless current_user?(@user)
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,11 @@
 module SessionsHelper
 	def sign_in(user)
 		cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-		self.current_user = user
+		@current_user = user
+	end
+	
+	def current_user
+	  @current_user ||= user_from_remember_token
 	end
 	
 	def current_user
@@ -10,6 +14,26 @@ module SessionsHelper
 	
 	def signed_in?
 		!current_user.nil?
+	end
+	
+	def sign_out
+		cookies.delete(:remember_token)
+		@current_user = nil
+	end
+	
+	def current_user?(user)
+		user == current_user
+	end
+
+	
+	def deny_access
+		store_location
+		redirect_to signin_path, :notice => "Please sign in to access this page."
+    end
+	
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		clear_return_to
 	end
 
 	private
@@ -20,5 +44,13 @@ module SessionsHelper
 
 		def remember_token
 			cookies.signed[:remember_token] || [nil, nil]
+		end
+		
+		def store_location
+		  session[:return_to] = request.fullpath
+		end
+
+		def clear_return_to
+		  session[:return_to] = nil
 		end
 end
